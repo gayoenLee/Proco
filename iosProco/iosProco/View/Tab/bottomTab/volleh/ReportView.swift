5357c33bc7f9276866fc3b82d2dc996da31b9f65
gayoenLee <leeedho67@gamil.com>
2021-06-16 11:21:00 +0900

    [MOD]신고 하기 뷰 수정

--- iosProco/iosProco/View/Tab/bottomTab/volleh/ReportView.swift
+++ iosProco/iosProco/View/Tab/bottomTab/volleh/ReportView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 import SocketIO
 
 struct ReportView: View {
-    
+    @Environment(\.presentationMode) var presentation
+
     @Binding var show_report : Bool
     //채팅방에서 신고하는지, 카드를 신고하는지 onappear에서 타입을 받아서 예외처리.
     @State var type : String
@@ -28,10 +29,38 @@ struct ReportView: View {
     @State private var report_ok_chatroom = false
     
     var body: some View {
+        
     VStack{
+        
+        //상단 제목 라인
+        HStack{
+            
+            Button(action: {
+                   
+                self.presentation.wrappedValue.dismiss()
+                
+            }, label: {
+                
+                Image("left")
+                    .resizable()
+                    .frame(width: 8.51, height: 17)
+            })
+            .padding(.leading, UIScreen.main.bounds.width/20)
+            
+            Spacer()
+            
+            Text("신고하기")
+                .font(.custom(Font.t_extra_bold, size: 20))
+                .foregroundColor(.proco_black)
+                .padding(.trailing, UIScreen.main.bounds.width/20)
+            
+            Spacer()
+    
+        }
+        .padding()
+        
         ZStack{
             ReportRadioButtons(selected: self.$selected, report_content: self.$report_content)
-            
         }
 
         Button(action: {
@@ -56,10 +85,16 @@ struct ReportView: View {
                 print("채팅방 회원 신고일 때")
                 let unique_idx = "\(socket_manager.enter_chatroom_idx),\(selected_user_idx)"
                 self.socket_manager.send_reports(kinds: "채팅방회원", unique_idx: unique_idx, report_kinds: self.selected, content: self.report_content)
-            
             }
         }){
             Text("확인")
+                .font(.custom(Font.t_extra_bold, size: 15))
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding()
+                .foregroundColor(.proco_white)
+                .background(Color.proco_black)
+                .cornerRadius(25)
+                .padding([.leading, .trailing], UIScreen.main.bounds.width/25)
         }
         .alert(isPresented: $main_vm.show_result_alert){
             switch main_vm.request_result_alert{
@@ -72,21 +107,13 @@ struct ReportView: View {
             case .fail:
                 return Alert(title: Text("신고하기"), message: Text("다시 시도해주세요"), dismissButton: Alert.Button.default(Text("확인"), action: {
                     
-                    
                 }))
             }
         }
-        
     }.onAppear{
         print("신고하기 뷰 나타남: \(self.type)")
-        
-        
+        }
     }
-    .onDisappear{
-        
-    }
-    }
-    
 }
 
 struct ReportRadioButtons: View{
@@ -106,18 +133,28 @@ struct ReportRadioButtons: View{
                 }){
                     HStack{
                         Text(reason)
+                            .font(.custom(Font.n_regular, size: 10))
+                            .foregroundColor(Color.proco_black)
+                        
                         Spacer()
                         
-                        ZStack{
-                            Circle().fill(Color.yellow.opacity(0.5)).frame(width: UIScreen.main.bounds.width/20, height: UIScreen.main.bounds.width/20)
+                        HStack{
                             if self.selected == reason{
-                                Circle().stroke(Color.yellow, lineWidth: 4).frame(width: UIScreen.main.bounds.width/30, height: UIScreen.main.bounds.width/30)
+                                Image("checked_small")
+                                    .resizable()
+                                    .frame(width: 6, height: 6)
+                            }else{
+                                Image("check_small")
+                                    .resizable()
+                                    .frame(width: 6, height: 6)
                             }
                         }
                     }
-                    
                 }
             }
+            
+            if selected == "기타"{
+            
             HStack(alignment: .firstTextBaseline, spacing: 20){
                 
             Text("신고내용")
@@ -131,11 +168,10 @@ struct ReportRadioButtons: View{
                       .foregroundColor(.black)
                 .frame(width: UIScreen.main.bounds.width*0.9, height: UIScreen.main.bounds.width, alignment: .center)
                             
-         
+            }
         }.padding(.vertical)
         .padding(.horizontal, UIScreen.main.bounds.width/20)
     }
-    
 }
 
 var report_menus = ["성적인 게시물", "폭력 또는 혐오 게시물", "스팸 또는 불법 게시물", "기타"]
