ca67a66c84cbfc9c9ede14fd32bc7090bd32874c
gayoenLee <leeedho67@gamil.com>
2021-06-21 10:42:17 +0900

    [MOD]커스텀 네비게이션 바 백버튼 추가

--- iosProco/iosProco/NavigationBarModifier.swift
+++ iosProco/iosProco/NavigationBarModifier.swift
@@ -14,7 +14,8 @@ struct NavigationBarModifier : ViewModifier{
     var title: String?
     var ui_img : UIImage{
         UIImage(named: "\(String(describing: background_img))")!
-    } 
+    }
+    var back_btn_img : UIImage = (UIImage(named: "left")?.withRenderingMode(.alwaysOriginal))!
     
     init(ui_img: UIImage, title: String) {
       let coloredAppearance = UINavigationBarAppearance()
@@ -23,19 +24,31 @@ struct NavigationBarModifier : ViewModifier{
         coloredAppearance.configureWithTransparentBackground()
       coloredAppearance.backgroundImage = ui_img
         coloredAppearance.backgroundImageContentMode = .redraw
-                     
+        coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor.red]
+        coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.red]
+        //coloredAppearance.backButtonAppearance.configureWithDefault(for: .done)
+        coloredAppearance.setBackIndicatorImage(UIImage(named: "left"), transitionMaskImage: UIImage(named: "left"))
+       
+       
+        UIBarButtonItem.appearance().setBackButtonTitlePositionAdjustment(UIOffset(horizontal: 0, vertical: -80.0), for: .default)
       UINavigationBar.appearance().standardAppearance = coloredAppearance
       UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
       UINavigationBar.appearance().compactAppearance = coloredAppearance
+        UINavigationBar.appearance().tintColor = .white
+        
     }
 
     func body(content: Content) -> some View {
+      
       content
+        
+    
     }
   }
 
 extension View {
-  func navigationBarColor(background_img: String, title: String) -> some View {
+  func navigationBarColor(background_img: String, title: String,
+                          displayMode : NavigationBarItem.TitleDisplayMode = .automatic) -> some View {
     self.modifier(NavigationBarModifier(ui_img: UIImage(named: "\(background_img)")!, title: title))
   }
 }
