d0cceaef6ebc7eb7b263851182f29843add01973
gayoenLee <leeedho67@gamil.com>
2021-06-27 10:26:25 +0900

    [MOD]피드에서 친구 아닌 경우 뷰에서 친구 신청 버튼 클릭후 알림창 띄우는 메소드 수정, 뷰 수정

--- iosProco/iosProco/View/Tab/bottomTab/feed/FeedNotFriendView.swift
+++ iosProco/iosProco/View/Tab/bottomTab/feed/FeedNotFriendView.swift
@@ -28,50 +28,15 @@ struct FeedLimitedView: View {
                 
                 Button(action: {
                     print("친구 신청 버튼 클릭")
+                    self.main_vm.friend_request_result_alert_func(main_vm.friend_request_result_alert)
+                    
                     //친구 요청 통신
                     self.main_vm.add_friend_request(f_idx: SimSimFeedPage.calendar_owner_idx!)
-                    self.main_vm.request_result_alert_func(main_vm.request_result_alert)
-                    
                 }){
                     
-                    Rectangle()
-                        .foregroundColor(Color.green)
-                        .overlay(
-                            Text("친구 신청")
-                                .font(.system(size: 15))
-                        )
-                }
-                .alert(isPresented: $main_vm.show_result_alert){
-                    switch main_vm.request_result_alert{
-                    case .no_friends, .denied:
-                        return Alert(title: Text("친구 추가하기"), message: Text("없는 사용자입니다"), dismissButton: Alert.Button.default(Text("확인"), action: {
-                            
-                        }))
-                    case .request_wait:
-                        return Alert(title: Text("친구 추가하기"), message: Text("친구 요청된 사용자입니다"), dismissButton: Alert.Button.default(Text("확인"), action: {
-                            
-                        }))
-                    case .requested:
-                        return Alert(title: Text("친구 추가하기"), message: Text("친구 요청된 사용자입니다"), dismissButton: Alert.Button.default(Text("확인"), action: {
-                            
-                        }))
-                    case .already_friend:
-                        return Alert(title: Text("친구 추가하기"), message: Text("이미 친구 상태인 사용자입니다"), dismissButton: Alert.Button.default(Text("확인"), action: {
-                            
-                        }))
-                    case .myself:
-                        return Alert(title: Text("친구 추가하기"), message: Text("내 번호입니다"), dismissButton: Alert.Button.default(Text("확인"), action: {
-                            
-                        }))
-                    case .success:
-                        return Alert(title: Text("친구 추가하기"), message: Text("친구 신청이 완료됐습니다."), dismissButton: Alert.Button.default(Text("확인"), action: {
-                            
-                        }))
-                    case .fail:
-                        return Alert(title: Text("친구 추가하기"), message: Text("다시 시도해주세요"), dismissButton: Alert.Button.default(Text("확인"), action: {
-                            
-                        }))
-                    }
+                    Text("친구 신청")
+                        .font(.system(size: 15))
+                    
                 }
             }
         }
