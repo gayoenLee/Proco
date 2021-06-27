bc48c655f6e8c22ef8574b5d1040b8626e84fba7
gayoenLee <leeedho67@gamil.com>
2021-06-27 10:23:29 +0900

    [MOD]신청한 모임 카드 디자인 수정, 코드 정렬

--- iosProco/iosProco/View/Tab/bottomTab/volleh/group_volleh/ApplyMeetingCard.swift
+++ iosProco/iosProco/View/Tab/bottomTab/volleh/group_volleh/ApplyMeetingCard.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ApplyMeetingCard: View {
     @ObservedObject var main_vm : GroupVollehMainViewmodel
     @Binding var apply_card : MyApplyMeetingStruct
     @State private var expiration_at = ""
-
+    let img_processor = ResizingImageProcessor(referenceSize: CGSize(width: 43, height: 43)) |> RoundCornerImageProcessor(cornerRadius: 5)
+    
+    //모임 카드 이미지가 없는 경우 왼쪽에 공백 생기는 문제 해결 위함.
+    var has_card_img : Bool{
+        if self.apply_card.card_photo_path == "" || self.apply_card.card_photo_path == nil{
+            return false
+        }else{
+            return true
+        }
+    }
+    
     var body: some View {
         //카드 1개
         HStack{
@@ -20,10 +31,11 @@ struct ApplyMeetingCard: View {
             VStack{
                 HStack{
                     
-                    if apply_card.card_photo_path != nil || apply_card.card_photo_path != "" {
+                    if has_card_img{
                         card_img
                     }
-                   category_and_title
+                    category_and_title
+                        .padding(.leading)
                     Spacer()
                     card_date
                 }
@@ -31,7 +43,7 @@ struct ApplyMeetingCard: View {
                     
                     current_user_num
                     Spacer()
-                    lock
+                    
                 }
                 
                 HStack{
@@ -43,7 +55,7 @@ struct ApplyMeetingCard: View {
         }
         .onAppear{
             self.expiration_at = String.dot_form_date_string(date_string: apply_card.expiration_at!)
-           print("날짜 확인: \(self.expiration_at)")
+            print("날짜 확인: \(self.expiration_at)")
         }
         //화면 하나에 카드 여러개 보여주기 위해 조정하는 값
         .frame(width: UIScreen.main.bounds.width*0.95, height: UIScreen.main.bounds.width/2.5)
@@ -63,18 +75,26 @@ extension ApplyMeetingCard{
     
     var category_and_title : some View{
         VStack{
-            Capsule()
-                .foregroundColor(apply_card.tags![0].tag_name == "사교/인맥" ? .proco_yellow : apply_card.tags![0].tag_name == "게임/오락" ? .proco_pink : apply_card.tags![0].tag_name == "문화/공연/축제" ? .proco_olive : apply_card.tags![0].tag_name == "운동/스포츠" ? .proco_green : apply_card.tags![0].tag_name == "취미/여가" ? .proco_mint : apply_card.tags![0].tag_name == "스터디" ? .proco_blue : .proco_red)
-                .frame(width: UIScreen.main.bounds.width*0.15, height: UIScreen.main.bounds.width/17)
-                .overlay(
-            Text("\(apply_card.tags![0].tag_name)")
-                .font(.custom(Font.t_extra_bold, size: 13))
-                .foregroundColor(.proco_white)
-                )
             
-            Text("\(apply_card.title!)")
-                .font(.custom(Font.n_extra_bold, size: 18))
-                .foregroundColor(.proco_black)
+            HStack{
+                Capsule()
+                    .foregroundColor(apply_card.tags![0].tag_name == "사교/인맥" ? .proco_yellow : apply_card.tags![0].tag_name == "게임/오락" ? .proco_pink : apply_card.tags![0].tag_name == "문화/공연/축제" ? .proco_olive : apply_card.tags![0].tag_name == "운동/스포츠" ? .proco_green : apply_card.tags![0].tag_name == "취미/여가" ? .proco_mint : apply_card.tags![0].tag_name == "스터디" ? .proco_blue : .proco_red)
+                    .frame(width: UIScreen.main.bounds.width*0.2, height: UIScreen.main.bounds.width/17)
+                    .overlay(
+                        Text("\(apply_card.tags![0].tag_name)")
+                            .font(.custom(Font.t_extra_bold, size: 12))
+                            .foregroundColor(.proco_white)
+                    )
+                
+                Spacer()
+            }
+            
+            HStack{
+                Text("\(apply_card.title!)")
+                    .font(.custom(Font.n_extra_bold, size: 15))
+                    .foregroundColor(.proco_black)
+                Spacer()
+            }
         }
     }
     
@@ -85,10 +105,10 @@ extension ApplyMeetingCard{
                 .frame(width: UIScreen.main.bounds.width*0.3, height: UIScreen.main.bounds.width/14)
                 .overlay(
                     
-            //카드 만료일
+                    //카드 만료일
                     Text("\(self.expiration_at)")
-                .font(.custom(Font.n_extra_bold, size: 15))
-                .foregroundColor(.proco_white))
+                        .font(.custom(Font.n_extra_bold, size: 15))
+                        .foregroundColor(.proco_white))
                 .padding(.top, UIScreen.main.bounds.width/20)
             Spacer()
         }
@@ -99,6 +119,7 @@ extension ApplyMeetingCard{
             Image(self.apply_card.cur_user ?? 0 > 0 ? "meeting_user_num_icon" : "")
                 .resizable()
                 .frame(width: 11, height: 11)
+            
             Text(self.apply_card.cur_user ?? 0  > 0 ? "\(self.apply_card.cur_user!)명" : "")
                 .font(.custom("", size: 13))
                 .foregroundColor(.proco_black)
@@ -106,21 +127,13 @@ extension ApplyMeetingCard{
         .padding(.leading)
     }
     
-    var lock : some View{
-        HStack{
-            Image(self.apply_card.lock_state == 0 ? "lock_public" : "lock_private")
-                .resizable()
-                .frame(width: 14, height: 11)
-        }
-        .padding(.trailing)
-    }
     
     var like_icon_num : some View{
         HStack{
             Button(action: {
                 
                 if self.apply_card.like_state == 0{
-                   
+                    
                     print("모임 카드 좋아요 클릭")
                     self.main_vm.send_like_card(card_idx: self.apply_card.card_idx!)
                     
@@ -130,15 +143,15 @@ extension ApplyMeetingCard{
                 }
                 
             }){
-            Image(apply_card.like_state == 0 ? "heart" : "heart_fill")
-            .resizable()
-            .frame(width: 14, height: 12)
-            
+                Image(apply_card.like_state == 0 ? "heart" : "heart_fill")
+                    .resizable()
+                    .frame(width: 14, height: 12)
+                
             }
-            Text(apply_card.like_count > 0 ? "좋아요\(apply_card.like_count)개" : "")
+            Text(apply_card.like_count ?? 0 > 0 ? "좋아요\(apply_card.like_count!)개" : "")
                 .font(.custom(Font.t_extra_bold, size: 12))
-            .foregroundColor(.proco_red)
-
+                .foregroundColor(.proco_black)
+            
         }
         .padding([.leading, .bottom])
         .onReceive(NotificationCenter.default.publisher(for: Notification.clicked_like), perform: {value in
@@ -155,21 +168,21 @@ extension ApplyMeetingCard{
                     
                     if card_idx == self.apply_card.card_idx{
                         
-                        self.apply_card.like_count += 1
+                        self.apply_card.like_count! += 1
                         self.apply_card.like_state = 1
-
-                }
+                        
+                    }
                     
                 }else if check_result as! String == "canceled_ok"{
                     let card = user_info["card_idx"] as! String
                     let card_idx = Int(card)
                     print("좋아요 취소한 idx: \(card_idx)")
                     if card_idx == self.apply_card.card_idx{
-                        self.apply_card.like_count -= 1
+                        self.apply_card.like_count! -= 1
                         self.apply_card.like_state = 0
                         
+                    }
                 }
-            }
             }
         })
     }
