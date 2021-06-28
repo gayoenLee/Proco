8a7f6480a26e88f83d6ef6eb2d3629e90c360d79
gayoenLee <leeedho67@gamil.com>
2021-06-28 14:43:53 +0900

    [MOD]피드 - 관심있어요 유저 한 명 뷰 수정, 유저 클릭시 피드 페이지 이동 전 데이터 저장 추가

--- iosProco/iosProco/View/Tab/bottomTab/feed/InterestUserCell.swift
+++ iosProco/iosProco/View/Tab/bottomTab/feed/InterestUserCell.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct InterestUserCell: View {
     
@@ -13,26 +14,52 @@ struct InterestUserCell: View {
     @StateObject var main_vm : CalendarViewModel
     //관심있어요한 사람 한명 클릭시 피드페이지로 이동하는 값.
     @State private var show_friend_feed: Bool = false
+    let scale = UIScreen.main.scale
+    let img_processor = ResizingImageProcessor(referenceSize: CGSize(width: 50, height: 50)) |> RoundCornerImageProcessor(cornerRadius: 40)
     
     var body: some View {
         HStack{
             
             NavigationLink("",destination: SimSimFeedPage(main_vm: self.main_vm), isActive: self.$show_friend_feed)
             
-            Image(systemName: (interest_users_model.profile_photo_path == "" ? "person" : interest_users_model.profile_photo_path) ?? "person")
-                .resizable()
-                .frame(width: UIScreen.main.bounds.width/20, height: UIScreen.main.bounds.width/20)
-                .foregroundColor(Color.black)
-
-            Spacer()
+            if interest_users_model.profile_photo_path == "" || interest_users_model.profile_photo_path == nil{
+                
+                Image("main_profile_img" )
+                    .resizable()
+                    .frame(width: 50, height:50)
+                
+            }else{
+                
+                KFImage(URL(string: interest_users_model.profile_photo_path!))
+                    .loadDiskFileSynchronously()
+                    .cacheMemoryOnly()
+                    .fade(duration: 0.25)
+                    .setProcessor(img_processor)
+                    .onProgress{receivedSize, totalSize in
+                        print("on progress: \(receivedSize), \(totalSize)")
+                    }
+                    .onSuccess{result in
+                        print("성공 : \(result)")
+                    }
+                    .onFailure{error in
+                        print("실패 이유: \(error)")
+                    }
+            }
             
             Text(interest_users_model.nickname)
-                .font(.system(size: 10))
-                .foregroundColor(Color.black)
+                .font(.custom(Font.n_extra_bold, size: 12))
+                .foregroundColor(Color.proco_black)
+            Spacer()
+
         }
         .padding()
         .onTapGesture {
             print("유저 클릭 이벤트: \(interest_users_model.idx)")
+            
+            self.main_vm.calendar_owner.profile_photo_path = interest_users_model.profile_photo_path ?? ""
+            self.main_vm.calendar_owner.user_idx = interest_users_model.idx
+            self.main_vm.calendar_owner.watch_user_idx = Int(self.main_vm.my_idx!)!
+            
            //피드 페이지로 이동하는 값 변경.
             self.show_friend_feed.toggle()
         }
