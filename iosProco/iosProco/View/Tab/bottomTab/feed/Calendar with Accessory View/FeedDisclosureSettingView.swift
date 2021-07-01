02c136e56e5557d609127c2b8d1bf00de5f4b899
gayoenLee <leeedho67@gamil.com>
2021-07-01 21:18:24 +0900

    [MOD]피드에서 알림 설정값 int값 수정

--- iosProco/iosProco/View/Tab/bottomTab/feed/Calendar with Accessory View/FeedDisclosureSettingView.swift
+++ iosProco/iosProco/View/Tab/bottomTab/feed/Calendar with Accessory View/FeedDisclosureSettingView.swift
@@ -23,10 +23,10 @@ struct FeedDisclosureSettingView: View {
         .onAppear{
 
             //설정 페이지 들어올 때 user info model에 저장했던 calendar public state가져옴.
-            if self.main_vm.user_info_model.calendar_public_state == 0{
+            if self.main_vm.user_info_model.calendar_public_state == 1{
                 self.selected = "전체 공개"
                 print("전체 공개임")
-            }else if self.main_vm.user_info_model.calendar_public_state == 1{
+            }else if self.main_vm.user_info_model.calendar_public_state == 2{
                 
                 self.selected = "카드만 공개"
                 print("카드만 공개임")
@@ -39,21 +39,21 @@ struct FeedDisclosureSettingView: View {
         }
         .onDisappear{
             
-            if self.selected == "전체 공개" && self.main_vm.user_info_model.calendar_public_state != 0{
+            if self.selected == "전체 공개" && self.main_vm.user_info_model.calendar_public_state != 1{
                 print("이전 값과 달라서 편집 통신")
-                self.main_vm.user_info_model.calendar_public_state = 0
-                self.main_vm.edit_calendar_disclosure_setting(calendar_public_state: self.main_vm.user_info_model.calendar_public_state)
-                
-            }else if self.selected == "카드만 공개" &&  self.main_vm.user_info_model.calendar_public_state != 1{
-                print("이전 값과 달라서 편집 통신")
-
                 self.main_vm.user_info_model.calendar_public_state = 1
                 self.main_vm.edit_calendar_disclosure_setting(calendar_public_state: self.main_vm.user_info_model.calendar_public_state)
                 
-            }else if self.selected == "비공개" &&   self.main_vm.user_info_model.calendar_public_state != 2{
+            }else if self.selected == "카드만 공개" &&  self.main_vm.user_info_model.calendar_public_state != 2{
                 print("이전 값과 달라서 편집 통신")
 
                 self.main_vm.user_info_model.calendar_public_state = 2
+                self.main_vm.edit_calendar_disclosure_setting(calendar_public_state: self.main_vm.user_info_model.calendar_public_state)
+                
+            }else if self.selected == "비공개" &&   self.main_vm.user_info_model.calendar_public_state != 0{
+                print("이전 값과 달라서 편집 통신")
+
+                self.main_vm.user_info_model.calendar_public_state = 0
                 self.main_vm.edit_calendar_disclosure_setting(calendar_public_state: self.main_vm.user_info_model.calendar_public_state)
                 
             }
