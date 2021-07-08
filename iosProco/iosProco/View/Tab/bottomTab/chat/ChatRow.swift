4c36d6c54fb5a90bea044ae0716f1e04f59694d3
gayoenLee <leeedho67@gamil.com>
2021-07-08 10:36:52 +0900

    [MOD]채팅 메세지 한개 뷰 디자인 수정

--- iosProco/iosProco/View/Tab/bottomTab/chat/ChatRow.swift
+++ iosProco/iosProco/View/Tab/bottomTab/chat/ChatRow.swift
@@ -45,10 +45,10 @@ struct ChatRow : View{
     @Binding var image_url: String?
     
     var body: some View{
-        HStack{
+        HStack(alignment: .bottom){
             //1.내 메세지
             if msg.myMsg{
-                Spacer(minLength: 25)
+                Spacer()
                 //-1일 경우 메세지를 서버에서 보낸 후 응답받기 전일 경우, -2는 메세지 데이터 끊은 상태에서 보냈을 때
                 if msg.message_idx == -1 || msg.message_idx == -2 {
                     //아이콘, 메세지
@@ -84,25 +84,30 @@ struct ChatRow : View{
                             .onTapGesture{
                                 self.image_url = msg.message!
                                 self.show_img_bigger = true
-                                
                             }
-                        
-                        //일반 메세지일 때
                     }
+                    //일반 메세지일 때
                     else{
+                      
                         //메세지 버블, 텍스트뷰
                         ZStack{
-                            Image("my_big_bubble")
+                            
+                            Image("my_msg_bubble")
                                 .resizable(capInsets: EdgeInsets(top: 20, leading: 27, bottom: 20, trailing: 27))
                             
                             Text(msg.message!)
                                 .font(.custom(Font.n_bold, size: UIScreen.main.bounds.width/25))
                                 .foregroundColor(.proco_black)
-                                .padding(.horizontal, UIScreen.main.bounds.width/30)
-                                .padding(.vertical, UIScreen.main.bounds.width/30)
+                                .padding(.horizontal, UIScreen.main.bounds.width/40)
+                                .padding(.vertical, UIScreen.main.bounds.width/40)
                                 .layoutPriority(1)
+                                .lineLimit(nil)
+                                .multilineTextAlignment(.leading)
+                           
+                              
                         }
-                        .frame( maxWidth: UIScreen.main.bounds.width*0.6, maxHeight: .infinity)
+                        .padding(.trailing)
+                        .fixedSize(horizontal:false , vertical : true)
                     }
                 }
             }
@@ -113,12 +118,15 @@ struct ChatRow : View{
                     .padding(.all)
                     .background(Color.black.opacity(0.06))
                     .cornerRadius(15)
+                    .lineLimit(50)
+                    .multilineTextAlignment(.leading)
+                    .fixedSize(horizontal:false , vertical : true)
                 Spacer()
                 
             }
             //3.상대방 메세지
             else{
-                HStack(alignment:.lastTextBaseline){
+                HStack(alignment: .bottom){
                     //같은 사람이 연속으로 보낸 메세지이지만 그 연속된 순서중 마지막이 아닌 경우
                     if msg.is_same_person_msg == true && msg.is_last_consecutive_msg == false{
                         Spacer()
@@ -126,7 +134,11 @@ struct ChatRow : View{
                         message_view
 
                         VStack{
+                            Spacer()
+                            HStack{
                             friend_read_num
+                            Spacer()
+                            }
                         }
                         //같은 사람이 연속으로 보냈고 연속된 순서의 마지막 메세지인 경우
                     }else if msg.is_same_person_msg == true && msg.is_last_consecutive_msg == true{
@@ -135,41 +147,61 @@ struct ChatRow : View{
                         message_view
 
                         VStack{
-                            
+                            Spacer()
+                            HStack{
                             friend_read_num
-
+                                Spacer()
+                            }
+                            HStack{
                             friend_send_time
+                                Spacer()
+                            }
                         }
                         //제일 처음으로 보낸 메세지인 경우
                     } else if msg.is_same_person_msg == false && msg.is_last_consecutive_msg == false{
                         
+                        HStack(alignment: .top){
                         //프로필 이미지
                         friend_profile_img
+                         
                         VStack(alignment: .leading){
                             friend_nickname
                             message_view
 
                         }
-                        HStack{
+                        }
+                        VStack{
                             Spacer()
+                        HStack{
                             friend_read_num
+                            Spacer()
+                        }
                         }
                     }else if msg.is_same_person_msg == false && msg.is_last_consecutive_msg == true{
                         //프로필 이미지
+                        HStack(alignment: .top){
                         friend_profile_img
-
+                        
                         VStack(alignment: .leading){
                             friend_nickname
                             message_view
 
                         }
-                        HStack{
+                        }
+                        VStack{
                             Spacer()
+                        HStack{
                             friend_read_num
+                            Spacer()
+                        }
+                        
+                        HStack{
                             friend_send_time
+                            Spacer()
+                        }
                         }
                     }
-                    Spacer(minLength: 25)
+                    Spacer()
                 }
             }
         }
@@ -247,7 +279,8 @@ private extension ChatRow{
         .frame(maxWidth: UIScreen.main.bounds.width*0.6, maxHeight: .infinity)
         .onTapGesture {
             print("동적링크 메세지 클릭: \(String(describing: msg.message))")
-            
+            SockMgr.socket_manager.is_dynamic_link = true
+            SockMgr.socket_manager.detail_to_invite = false
             //초대하려는 채팅방idx
             let info = msg.message
             NotificationCenter.default.post(name: Notification.clicked_invite_link, object: nil, userInfo: ["clicked_invite_link" : "ok", "info": String(info!) ])
@@ -320,37 +353,47 @@ private extension ChatRow{
             if msg.is_same_person_msg == false && msg.is_last_consecutive_msg == false{
                 VStack{
                     Spacer()
-                 
+                    HStack{
+                        Spacer()
                     Text(String(msg.read_num) == "0" ? "" : String(msg.read_num))
                         .font(.custom(Font.n_extra_bold, size: 9))
                         .foregroundColor(.proco_yellow)
+                    }
                 }
                 
                 //1분 안에 연속으로 보낸 메세지이고 연속인데 마지막인 경우
             }else if msg.is_same_person_msg == true && msg.is_last_consecutive_msg == true{
                 VStack{
                     Spacer()
-                   
+                    HStack{
+                        Spacer()
                     Text(String(msg.read_num) == "0" ? "" : String(msg.read_num))
                         .font(.custom(Font.n_extra_bold, size: 9))
                         .foregroundColor(.proco_yellow)
-                    
+                    }
+                    HStack{
+                        Spacer()
                     Text("\(msg_time_converted)")
                         .font(.custom(Font.n_bold, size: 9))
                         .foregroundColor(.gray)
+                    }
                 }
                 //연속으로 보낸 메세지가 아닌 경우
             } else if msg.is_same_person_msg == false && msg.is_last_consecutive_msg == true{
                 VStack{
                     Spacer()
-                   
+                    HStack{
+                        Spacer()
                     Text(String(msg.read_num) == "0" ? "" : String(msg.read_num))
                         .font(.custom(Font.n_extra_bold, size: 9))
                         .foregroundColor(.proco_yellow)
-                    
+                    }
+                    HStack{
+                        Spacer()
                     Text("\(msg_time_converted)")
                         .font(.custom(Font.n_bold, size: 9))
                         .foregroundColor(.gray)
+                    }
                 }
             }
         }
@@ -361,10 +404,14 @@ private extension ChatRow{
         
         Text(msg.message!)
             .font(.custom(Font.n_bold, size: 12))
+            .multilineTextAlignment(.center)
+            .lineLimit(50)
+            .fixedSize(horizontal: false, vertical: true)
             .foregroundColor(.proco_black)
             .padding(.all)
             .background(Color.black.opacity(0.06))
             .cornerRadius(15)
+          
     }
     
     
@@ -376,13 +423,16 @@ private extension ChatRow{
                         .resizable(capInsets: EdgeInsets(top: 20, leading: 27, bottom: 20, trailing: 27))
                     
                     Text(msg.message!)
-                        .font(.custom(Font.n_bold, size: 12))
+                        .font(.custom(Font.n_bold, size: UIScreen.main.bounds.width/25))
                         .foregroundColor(.proco_black)
                         .padding(.horizontal, UIScreen.main.bounds.width/30)
-                        .padding(.vertical)
+                        .padding(.vertical, UIScreen.main.bounds.width/40)
                         .layoutPriority(1)
+                        .lineLimit(nil)
+                        .multilineTextAlignment(.leading)
                 }
-                .frame( maxWidth: UIScreen.main.bounds.width*0.6, maxHeight: .infinity)
+                .fixedSize(horizontal:false , vertical : true)
+
                 
             }else if msg.kinds == "P"{
                 
@@ -422,9 +472,12 @@ private extension ChatRow{
     }
     
     var friend_profile_img : some View{
-        Image((msg.profilePic == "" ? "main_profile_img" : msg.profilePic) ??  "main_profile_img")
-            .resizable()
-            .frame(width: 47.46, height: 47.46)
+        VStack{
+            Image((msg.profilePic == "" ? "main_profile_img" : msg.profilePic) ??  "main_profile_img")
+                .resizable()
+                .frame(width: 47.46, height: 47.46)
+            Spacer()
+        }
     }
     
     var friend_nickname : some View{
