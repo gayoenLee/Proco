fa2596f5810a3662d4a046a1818b698665df03d1
gayoenLee <leeedho67@gamil.com>
2021-07-08 10:37:09 +0900

    [MOD]채팅방 설정 뷰 디자인 수정

--- iosProco/iosProco/View/Tab/bottomTab/chat/ChatroomSettingView.swift
+++ iosProco/iosProco/View/Tab/bottomTab/chat/ChatroomSettingView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ChatroomSettingView: View {
     @Environment(\.presentationMode) var presentation
     @ObservedObject var socket : SockMgr
-
+    
     //var chatroom_name : String
     
     //채팅방 이름 변경 화면 이동 구분값.
@@ -20,7 +20,8 @@ struct ChatroomSettingView: View {
             //상단바
             HStack{
                 //뒤로 가기 버튼.
-                Button(action: {                    self.presentation.wrappedValue.dismiss()
+                Button(action: {
+                    self.presentation.wrappedValue.dismiss()
                     
                 }){
                     Image("left")
@@ -29,54 +30,51 @@ struct ChatroomSettingView: View {
                 }
                 Spacer()
                 Text("채팅방 설정")
-                    .font(.custom(Font.n_extra_bold, size: UIScreen.main.bounds.width/10))
+                    .font(.custom(Font.n_extra_bold, size: UIScreen.main.bounds.width/20))
                     .foregroundColor(.proco_black)
+                
                 Spacer()
                 
             }
             .padding()
             //이름 변경하는 화면 이동
             NavigationLink("",destination: ChatroomNameEditView(socket: self.socket).navigationBarHidden(true).navigationBarTitle("", displayMode: .inline), isActive: self.$go_edit_name)
-            
+            HStack{
+                Text("채팅방 이름")
+                    .font(.custom(Font.n_extra_bold, size: UIScreen.main.bounds.width/23))
+                    .foregroundColor(.proco_black)
+                Spacer()
+            }
+            .padding([.leading])
             //현재 채팅방 이름과 수정하기 버튼.
             HStack{
                 //친구, 모임, 일반 모두 채팅방 이름 커스텀 가능.
                 HStack{
                     if SockMgr.socket_manager.current_chatroom_info_struct.room_name == ""{
                         Text("채팅방 이름을 설정해보세요")
-                            .font(.custom(Font.n_extra_bold, size: UIScreen.main.bounds.width/15))
-                            .foregroundColor(.proco_black)
+                            .font(.custom(Font.n_extra_bold, size: UIScreen.main.bounds.width/25))
+                            .foregroundColor(.gray)
                     }else{
                         Text(SockMgr.socket_manager.current_chatroom_info_struct.room_name)
-                            .font(.custom(Font.n_extra_bold, size: UIScreen.main.bounds.width/15))
+                            .font(.custom(Font.n_extra_bold, size: UIScreen.main.bounds.width/25))
                             .foregroundColor(.proco_black)
                     }
                     
-                Spacer()
+                    Spacer()
                     
-                Button(action: {
-                    print("이름 변경하기 클릭")
-                    self.go_edit_name.toggle()
-                }){
-                    Image("pencil")
+                        Image("pencil")
+                            .resizable()
+                            .frame(width: UIScreen.main.bounds.width/15, height: UIScreen.main.bounds.width/15)
+                    
                 }
+                .padding()
+                .onTapGesture {
+                    self.go_edit_name.toggle()
                 }
             }
             
             Spacer()
-            Button(action: {
-                
-                
-            }){
-                Text("채팅방 나가기")
-                    .frame(maxWidth: .infinity)
-                    .font(.custom(Font.t_regular, size: UIScreen.main.bounds.width/10))
-                    .padding(UIScreen.main.bounds.width/20)
-                    .background(Color.proco_black)
-                    .foregroundColor(Color.proco_white)
-                    .padding()
-                
-            }
+            
         }
         .onAppear{
             print("채팅방 설정 화면 on appear")
