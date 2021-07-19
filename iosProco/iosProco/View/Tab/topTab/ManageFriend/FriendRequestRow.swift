68cf5aa8959e272338502b433bc969523bc8cc1f
gayoenLee <leeedho67@gamil.com>
2021-07-19 16:36:48 +0900

    [MOD]친구 요청 받은 row뷰 애니메이션 추가, onreceive 메인으로 이동시킴 ->index out of range에러 발생해서 수정한 것

--- iosProco/iosProco/View/Tab/topTab/ManageFriend/FriendRequestRow.swift
+++ iosProco/iosProco/View/Tab/topTab/ManageFriend/FriendRequestRow.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-//친구 신청 목록 row
+//친구 신청 받은 목록 row
 struct FriendRequestRow: View{
     @ObservedObject var manage_viewmodel : ManageFriendViewModel
     @State var request_struct : FriendRequestListStruct
@@ -30,55 +30,10 @@ struct FriendRequestRow: View{
               
                     decline_btn
                  
-                }else{
-                    if request_struct.processed == "수락됨"{
-                        accepted_btn
-                    }else if request_struct.processed == "거절됨"{
-                        declined_btn
-                    }
                 }
             }.padding()
         }
-        .onReceive(NotificationCenter.default.publisher(for: Notification.friend_request_event), perform: {value in
-            print("친구 요청 수락, 거절 노티 받음: \(value)")
-            
-            if let user_info = value.userInfo, let check_result = user_info["friend_request_event"]{
-                print("친구 요청 수락, 거절 노티 데이터: \(check_result)")
-                
-                //수락이벤트였을 경우
-                if check_result as! String == "accepted"{
-                    
-                    let friend_idx = user_info["friend_idx"] as! String
-                    
-                    if self.request_struct.idx == Int(friend_idx){
-                    let model_data = self.manage_viewmodel.friend_request_struct.first(where: {
-                        $0.idx! == Int(friend_idx)
-                    })!
-                    
-                    //친구 요청 리스트에서 제거는 안하고(index out of range에러 발생) 수락됨으로 뷰 변경, 친구 리스트에 추가
-                    manage_viewmodel.friend_list_struct.append(GetFriendListStruct(idx: model_data.idx, nickname: model_data.nickname, profile_photo: model_data.profile_photo_path, state: 0, kinds: "친구상태"))
-                    self.request_struct.processed = "수락됨"
-                    
-                    //전체 친구 수 +1
-                    self.friend_total_num += 1
-                    }
-                }
-                //거절 이벤트였을 경우
-                else if check_result as! String == "declined"{
-                    let friend_idx = user_info["friend_idx"] as! String
-                                        
-                    if self.request_struct.idx == Int(friend_idx){
-                        print("해당 친구인 경우")
-                    //친구 요청 리스트에서 제거하지 않고 뷰만 거절됨으로 변경.
-                    self.request_struct.processed = "거절됨"
-                    }
-                }
-                //두 이벤트 처리에 실패한 경우
-                else{
-                    print("수락, 거절 이벤트 노티 받았지만 두 경우 모두 아닌 else문")
-                }
-            }
-        })
+        .transition(.slide)
     }
 }
 
