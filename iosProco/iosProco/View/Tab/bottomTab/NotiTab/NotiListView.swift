4467eb938afab1ee920a5d2a055db43795bdae32
gayoenLee <leeedho67@gamil.com>
2021-07-22 19:19:41 +0900

    [MOD]알림 탭에서 피드 이동시 데이터 저장 추가

--- iosProco/iosProco/View/Tab/bottomTab/NotiTab/NotiListView.swift
+++ iosProco/iosProco/View/Tab/bottomTab/NotiTab/NotiListView.swift
@@ -24,6 +24,7 @@ struct NotiListView: View {
     @ObservedObject var friend_card_vm = FriendVollehMainViewmodel()
     //모임 카드 페이지로 이동 전 selected card idx를 저장하기 위해 필요.
     @ObservedObject var group_card_vm = GroupVollehMainViewmodel()
+    @ObservedObject var calendar_vm = CalendarViewModel()
     
     var body: some View {
         
@@ -31,6 +32,15 @@ struct NotiListView: View {
             TopNavBar(page_name: "알림")
             
             //스크롤뷰의 경우 리사이클러뷰처럼 뷰를 보이는 것만 그리는 것이 아님. -> 그래서 lazyvstack을 함께 써줘야 무한스크롤링 구현 가능
+            
+            if self.main_vm.noti_list_model.count <= 0{
+                HStack{
+                   Text("알림이 없습니다.")
+                    .font(.custom(Font.n_extra_bold, size: 15))
+                    .foregroundColor(.gray)
+                    
+                }
+            }else{
             ScrollView{
                 LazyVStack{
                     ForEach(0..<self.main_vm.noti_list_model.count, id: \.self){index in
@@ -38,16 +48,17 @@ struct NotiListView: View {
                         //한번에 갖고 와서 모델에 저장된 데이터의 마지막인 경우 is_last값 true
                         if index == self.main_vm.noti_list_model.count-1{
                             
-                            NotiRow(noti: self.main_vm.noti_list_model[index], main_vm: self.main_vm, go_friend_manage: self.$go_friend_manage, go_friend_card_detail: self.$go_friend_card_detail, go_group_card_detail: self.$go_group_card_detail, go_feed: self.$go_feed, friend_card_vm: self.friend_card_vm, group_card_vm: self.group_card_vm, is_last: true)
+                            NotiRow(noti: self.main_vm.noti_list_model[index], main_vm: self.main_vm, calendar_vm: self.calendar_vm, go_friend_manage: self.$go_friend_manage, go_friend_card_detail: self.$go_friend_card_detail, go_group_card_detail: self.$go_group_card_detail, go_feed: self.$go_feed, friend_card_vm: self.friend_card_vm, group_card_vm: self.group_card_vm, is_last: true)
                                 .padding([.leading, .trailing, .bottom])
                             
                         }else{
                             
-                            NotiRow(noti: self.main_vm.noti_list_model[index], main_vm: self.main_vm, go_friend_manage: self.$go_friend_manage, go_friend_card_detail: self.$go_friend_card_detail, go_group_card_detail: self.$go_group_card_detail, go_feed: self.$go_feed, friend_card_vm: self.friend_card_vm, group_card_vm: self.group_card_vm, is_last: false)
+                            NotiRow(noti: self.main_vm.noti_list_model[index], main_vm: self.main_vm, calendar_vm: self.calendar_vm, go_friend_manage: self.$go_friend_manage, go_friend_card_detail: self.$go_friend_card_detail, go_group_card_detail: self.$go_group_card_detail, go_feed: self.$go_feed, friend_card_vm: self.friend_card_vm, group_card_vm: self.group_card_vm, is_last: false)
                                 .padding([.leading, .trailing, .bottom])
                         }
                     }
                 }
+            }
             }
         }
         .navigationBarTitle("", displayMode: .inline)
@@ -62,6 +73,8 @@ struct NotiRow: View{
     
     @State var noti : NotiListModel
     @ObservedObject var main_vm : NotiListViewModel
+    //심심기간 관련 노티 화면 이동시 유저 정보 저장할 때 사용.
+    @ObservedObject var calendar_vm : CalendarViewModel
     
     let scale = UIScreen.main.scale
     let img_processor = ResizingImageProcessor(referenceSize: CGSize(width: 40, height: 40)) |> RoundCornerImageProcessor(cornerRadius: 25)
@@ -196,7 +209,7 @@ struct NotiRow: View{
                 
                 NavigationLink("",destination: GroupVollehCardDetail(main_vm: self.group_card_vm, socket: SockMgr.socket_manager, calendar_vm: CalendarViewModel()), isActive: self.$go_group_card_detail)
                 
-                NavigationLink("",destination: SimSimFeedPage(main_vm: CalendarViewModel(), view_router: ViewRouter()).navigationBarTitle("", displayMode: .inline).navigationBarHidden(true), isActive: self.$go_feed)
+                NavigationLink("",destination: SimSimFeedPage(main_vm: self.calendar_vm, view_router: ViewRouter()).navigationBarTitle("", displayMode: .inline).navigationBarHidden(true), isActive: self.$go_feed)
                     VStack{
                         //인디케이터 볼드 처리 위해 뷰빌더 메소드로 리턴
                         content()
@@ -213,7 +226,7 @@ struct NotiRow: View{
                     Spacer()
             }
         .onTapGesture {
-            print("노티 한 개 클릭: 종류 - \(noti.kinds!), unique_idx:\(noti.unique_idx)")
+            print("노티 한 개 클릭: \(noti)")
             
             //화면 이동
             //1. 친구관리 페이지 이동 - 친구요청, 친구수락
@@ -234,7 +247,13 @@ struct NotiRow: View{
                 return self.go_group_card_detail = true
                 
             case "관심친구심심기간":
+        
+                calendar_vm.calendar_owner.profile_photo_path = noti.image_path ?? ""
+                calendar_vm.calendar_owner.user_idx = noti.idx!
+                calendar_vm.calendar_owner.user_nickname = noti.content_indicator!
+                calendar_vm.calendar_owner.watch_user_idx = Int(UserDefaults.standard.string(forKey: "user_id")!)!
                 SimSimFeedPage.calendar_owner_idx = noti.unique_idx!
+                print("관심친구 심심기간 노티 클릭 후 데이터 확인: \(calendar_vm.calendar_owner)")
                 return self.go_feed = true
                 
             default:
