65294a36d6d0e77c5507392de629a0ef2a128087
gayoenLee <leeedho67@gamil.com>
2021-07-22 19:15:25 +0900

    [MOD]친구 카드 상세 페이지 데이터 모델 옵셔널 변경

--- iosProco/iosProco/Model/Volleh/Friend/FriendVollehCardDeatil.swift
+++ iosProco/iosProco/Model/Volleh/Friend/FriendVollehCardDeatil.swift
@@ -13,15 +13,15 @@ struct FriendVollehCardDetailModel: Codable{
     var result: String? = ""
     var card_idx: Int? = -1
     var kinds: String? = ""
-    var expiration_at: String = ""
-    var lock_state: Int = 0
-    var like_count: Int = 0
-    var like_state: Int = 0
+    var expiration_at: String? = ""
+    var lock_state: Int? = 0
+    var like_count: Int? = 0
+    var like_state: Int? = 0
     var tags: [Tags]? = []
     var share_list: [ShareList]? = []
-    var creator : Creator = Creator()
+    var creator : Creator? = Creator()
     //관심친구 여부
-    var is_favor_friend: Int = 0
+    var is_favor_friend: Int? = 0
     //카드 참가 유저 수
-    var cur_user : Int = 1
+    var cur_user : Int? = 1
 }
