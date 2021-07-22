0d752feeef989bd92090eef2de569649eacad161
gayoenLee <leeedho67@gamil.com>
2021-07-22 11:52:18 +0900

    [MOD]아이디, 비밀번호 찾기 뷰 인증번호 요청 후 alert창 띄우기 위한 변수명 수정, 확인버튼 disabled추가

--- iosProco/iosProco/View/FindIdPwd/FindIdPasswordView.swift
+++ iosProco/iosProco/View/FindIdPwd/FindIdPasswordView.swift
@@ -18,10 +18,10 @@ struct FindIdPasswordView: View{
     //인증번호 틀렸을 경우 경고 문구 나타내기 위한 구분값
     @State var show_warn = true
     //인증번호 요청 통신에 따라 alert창 나타내기 위한 변수
-    @State private var message_sended : Bool = false
-    @State private var message_failed : Bool = false
+    @State private var auth_send_result : Bool = false
+    @State private var result_message : String = ""
     //핸드폰 번호 형식 맞는지 구분 변수
-    @State private var is_phone_number_valid: Bool = true
+    @State private var is_phone_number_valid: Bool = false
     //인증번호 맞는지 체크
     @State private var is_auth_number_result: Bool = false
     //인증 확인 결과 alert창 나타내기 위한 변수
@@ -60,20 +60,8 @@ struct FindIdPasswordView: View{
                 HStack{
                     Text("+82").padding([.leading, .trailing], UIScreen.main.bounds.width/20)
                     
-                    TextField("휴대폰 번호", text: self.$phone_viewmodel.phone_number, onEditingChanged: {(is_changed)in
-                        if !is_changed{
-                            //phone_viewmodel에 있는 정규식 체크 메소드를 사용해 핸드폰 번호 양식 확인함.
-                            if self.phone_viewmodel.validator_phonenumber(self.phone_viewmodel.phone_number){
-                                self.is_phone_number_valid = true
-                                print("저장된 핸드폰 번호 확인 : \(self.phone_viewmodel.phone_number)")
-                                
-                            }else{
-                                self.is_phone_number_valid = false
-                                self.phone_viewmodel.phone_number = ""
-                            }
-                        }
-                    })
-                } .keyboardType(.phonePad)
+                    TextField("휴대폰 번호", text: self.$phone_viewmodel.phone_number)
+            } .keyboardType(.phonePad)
                 .padding()
                 .background(Color.proco_white)
                 .cornerRadius(25.0)
@@ -85,10 +73,11 @@ struct FindIdPasswordView: View{
                     Button(action:{
                         send_phone_auth()
                     }){
-                        Image("verify_active_btn")
+                        Image(self.phone_viewmodel.phone_is_valid ? "verify_active_btn" : "verify_inactive_btn")
                             .resizable()
                             .frame(width: 82, height: 33)
                     }
+                    .disabled(!self.phone_viewmodel.phone_is_valid)
                     .padding(.trailing, UIScreen.main.bounds.width/25)
                 }
             }
@@ -101,38 +90,53 @@ struct FindIdPasswordView: View{
                     .cornerRadius(25.0)
                     .shadow(color: .gray, radius: 2, x: 0, y: 2)
                     .padding([.leading, .trailing], UIScreen.main.bounds.width/20)
+                    .keyboardType(.phonePad)
+                    
                 
                 //핸드폰 번호 양식에 알맞지 않을 경우 나타나는 경고 문구
-                if self.is_auth_number_result{
+                if !self.is_auth_number_result{
                     if auth_result == "invalid"{
                         Text("잘못된 인증번호 입니다")
                             .font(.custom(Font.n_regular, size: 10))
                             .foregroundColor(Color.proco_red)
-                    }else{
-                        Text("이미 인증된 번호입니다")
+                            .padding([.leading])
+                    }
+                    else if auth_result == "auth_result"{
+                        Text("이미 인증번호를 보냈습니다.")
                             .font(.custom(Font.n_regular, size: 10))
                             .foregroundColor(Color.proco_red)
+                            .padding([.leading])
+                        
+                    }
+                    else{
+                        
+
                     }
                 }
                 Spacer()
                 HStack{
                     //다음 버튼 클릭시 인증번호 서버로 보내서 맞는지 확인하고 맞아야 이동 가능, self붙여야 데이터를 그대로 담아서 보낼 수 있음.
-                    NavigationLink(destination: ChangePasswordView(info_viewmodel: self.phone_viewmodel)   .navigationBarTitle("", displayMode: .inline)
-                                    .navigationBarHidden(true), isActive: $is_auth_number_result){
-                        
+                    NavigationLink("",destination: ChangePasswordView(info_viewmodel: self.phone_viewmodel)   .navigationBarTitle("", displayMode: .inline)
+                                    .navigationBarHidden(true), isActive: $is_auth_number_result)
+                    Button(action:{
+                        check_phone_auth()
+                        print("이메일 패스워드 이동하는 네비게이션 링크 클릭")
+                    }){
                         Text("확인")
                             .font(.custom(Font.t_extra_bold, size: 15))
                             .frame(minWidth: 0, maxWidth: .infinity)
                             .padding()
                             .foregroundColor(.proco_white)
-                            .background(Color.proco_black)
+                            //인증번호가 6자리이고 전화번호 형식이 맞을때만 활성화
+                            .background(self.phone_viewmodel.auth_num.count == 6 && (self.phone_viewmodel.phone_is_valid) ? Color.proco_black : Color.gray)
                             .cornerRadius(25)
                             .padding([.leading, .trailing], UIScreen.main.bounds.width/25)
-                        
-                    }.simultaneousGesture(TapGesture().onEnded{
-                        check_phone_auth()
-                        print("이메일 패스워드 이동하는 네비게이션 링크 클릭")
-                    })
+                    }
+                    .disabled(!(self.phone_viewmodel.auth_num.count == 6) && !(self.phone_viewmodel.phone_is_valid))
+                    .padding(.trailing, UIScreen.main.bounds.width/25)
+                    
+                    
+
                     //뷰가 사라질 때 값 저장하기
                     .onDisappear{
                         print("핸드폰 확인 : \(self.phone_viewmodel.phone_number)")
@@ -148,29 +152,36 @@ struct FindIdPasswordView: View{
                         .scaledToFill())
         //body끝
         //핸드폰 인증때 서버 통신 과정에서 알 수 없는 오류가 발생한 경우 띄우는 창
-        .alert(isPresented: $phone_auth_error){
-            Alert(title: Text(""), message: Text("인증요청을 다시 시도해주세요"), dismissButton: .default(Text("확인")))
-        }
-        .alert(isPresented: $message_sended){
-            Alert(title: Text("인증번호가 전송됐습니다."), dismissButton: .default(Text("확인")))
-        }
-        .alert(isPresented: $message_failed){
-            Alert(title: Text("인증번호 전송에 실패했습니다. 다시 시도해주세요"), dismissButton: .default(Text("확인")))
+
+        .alert(isPresented: $auth_send_result){
+            Alert(title: Text(result_message), dismissButton: .default(Text("확인")))
         }
     }
     //view 끝
     func send_phone_auth(){
         APIClient.find_id_pwd_phone_auth(phone_num: self.phone_viewmodel.phone_number, type: "miss_pwd", completion: {result in
             if result.exists(){
-                print("핸드폰 인증  뷰에서 \(result)")
+                print("핸드폰 인증 뷰에서 \(result)")
                 let result_string = result["result"].string
                 //이미 인증된 사람일 경우 alert창으로 알리기
-                if(result_string == "message_sended"){
-                    self.message_sended.toggle()
-                    //응답 : 인증문자 전송됨 : message sended
-                }else{
-                    self.message_failed.toggle()
+                switch result_string {
+                case "message sended":
+                    self.result_message = "인증번호가 전송되었습니다."
+                    break
+                case "already send auth message" :
+                    self.result_message = "이미 인증요청을 보냈습니다."
+                    break
+                case "message send error" :
+                    self.result_message = "요청을 처리하는데 문제가 발생했습니다."
+                    break
+                case "no user phone_num":
+                    self.result_message = "존재하지 않는 회원입니다."
+                    break
+                default :
+                    self.result_message = "알수 없는 응답 또는 에러"
+                    break
                 }
+                self.auth_send_result.toggle()
             }
         })
         
@@ -183,22 +194,28 @@ struct FindIdPasswordView: View{
             print("핸드폰 인증뷰에서 \(result)")
             
             if result.exists(){
-                is_auth_number_result = true
+                
                 let result_string = result["result"].string
-                //만약 인증번호가 일치한다는 결과일 경우
-                if (result_string ==  "invalid auth number"){
-                    //뷰모델에도 데이터 저장
-                    auth_result = "ok"
-                    //인증번호가 일치하지 않을 경우
-                }
-                else if result_string == "invalid auth num"{
-                    phone_viewmodel.phone_auth_ok = false
+                switch result_string {
+                case "invalid auth number":
                     auth_result = "invalid"
+                    break
+                case "no user phone_num":
+                    auth_result = "no user phone_num"
+                    break
+                case "ok":
+                    auth_result = "ok"
+                    is_auth_number_result = true
+                    break
+                default:
+                    auth_result = "error"
+                    break
                 }
-                //통신에 실패한 경우
+
             }else{
                 print("핸드폰 인증 뷰에서 \(Error.self)")
-                phone_auth_error.toggle()
+                self.result_message = "요청을 처리하는데 문제가 발생하였습니다."
+                auth_send_result.toggle()
                 
             }
         })
@@ -210,4 +227,3 @@ struct Message: Identifiable {
     let id = UUID()
     let text: String
 }
-
