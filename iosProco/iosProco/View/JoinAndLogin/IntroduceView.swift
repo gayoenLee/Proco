4dfe4bac9c3aa07acb03f3b2189b0ae0a2c706f3
gayoenLee <leeedho67@gamil.com>
2021-07-22 11:51:04 +0900

    [ADD]회원가입 완료 후 소개 페이지 추가

---
+++ iosProco/iosProco/View/JoinAndLogin/IntroduceView.swift
@@ -0,0 +1,36 @@
+//
+//  IntroduceView.swift
+//  proco
+//
+//  Created by 이은호 on 2021/07/21.
+//
+
+import SwiftUI
+
+struct IntroduceView: View {
+   
+
+    let url : String
+    @State private var go_main : Bool = false
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Button(action: {
+                    self.go_main = true
+                }){
+                    Image("card_dialog_close_icon")
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width/20, height: UIScreen.main.bounds.width/20)
+                }
+                
+                Spacer()
+            }
+            .padding([.top, .leading, .trailing])
+        SignupTermsWebView(url: URL(string: url)!)
+            NavigationLink("",destination: TabbarView(view_router: ViewRouter()).navigationBarHidden(true), isActive: self.$go_main)
+        }
+        .navigationBarHidden(true)
+    }
+}
+
