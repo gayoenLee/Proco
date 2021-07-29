41ab794573e58e63d175e00bf9d0560329f8f97d
gayoenLee <leeedho67@gamil.com>
2021-07-29 19:37:18 +0900

    [MOD]소셜 로그인 삭제로 뷰 스택 관리 수정

--- iosProco/iosProco/View/SplashView.swift
+++ iosProco/iosProco/View/SplashView.swift
@@ -12,8 +12,8 @@ struct SplashView: View {
     //사용자의 토큰 저장하는 클래스로 토큰 값 가져오기 위해 선언.
 
     //토큰으로 서버와 통신 후 어디 뷰로 이동할지 결정하기 위해 주는 구분값
-    @State private var go_to_login : Bool = false
-    @State private var go_to_main : Bool = false
+//    @State private var go_to_login : Bool = false
+//    @State private var go_to_main : Bool = false
     
     //스플래시 뷰가 스레드로 작동하기 때문에 끝난 것을 알 수 있게 해주는 구분값.
     @State var end_splash_active : Bool = false
@@ -26,12 +26,16 @@ struct SplashView: View {
         VStack{
             //스플래시뷰가 끝난 후 response에 따라 이동하는 뷰를 다르게 설정.
             if self.end_splash_active{
-                if go_to_login{
-                    
-                    LoginMenuView()
-                    
-                } else if go_to_main{
-                    TabbarView(view_router: self.view_router)
+                //친구랑 볼래 메인 화면
+                if self.view_router.init_root_view == "tab_main"{
+                    TabbarView()
+                }
+                //회원가입 완료 후 보여줄 화면 - 친구 초대 화면
+                else if self.view_router.init_root_view == "enrolled_friend"{
+                    EnrolledFriendListView()
+                }
+                else{
+                    NormalLoginView()
                 }
                 //스플래시 뷰 화면 구성
             }else{
@@ -53,16 +57,16 @@ struct SplashView: View {
                     //저장된 리프레시 토큰이 없을 경우
                     if UserDefaults.standard.string(forKey: "refresh_token")  == ""{
                         print("저장된 토큰 없음")
-                        go_to_login.toggle()
+                        self.view_router.init_root_view = "origin"
                         
                         //토큰이 있으면 서버로 토큰 전송
                     }else if refresh_token_value != nil {
                         print("저장된 토큰 있음")
-                    
+                        self.view_router.init_root_view = "tab_main"
                         check_refresh_token()
                     }else{
                         print("저장된 토큰 없는데 그 외 경우")
-                        go_to_login.toggle()
+                        self.view_router.init_root_view = "origin"
                     }
                 }
             }
@@ -86,9 +90,11 @@ struct SplashView: View {
                         print("액세스토큰 받음")
                         UserDefaults.standard.set(result_access, forKey: "access_token")
                         SockMgr.socket_manager.establish_connection()
-
+                        
+                        //초기메인화면 설정값을 탭뷰로 보이기 위해 사용
+                        self.view_router.init_root_view = "tab_main"
                         //토큰이 있으므로 메인화면으로 보내기
-                        go_to_main.toggle()
+                      //  go_to_main.toggle()
                     }
                     
                     //리프레시 토큰의 기간이 얼마 남지 않았으면 다시 발급 받기
@@ -98,24 +104,24 @@ struct SplashView: View {
                         print("토큰 refresh")
                         if let result_access = result["access_token"].string{
                             UserDefaults.standard.set(result_access, forKey: "access_token")
-                            go_to_main.toggle()
+                            self.view_router.init_root_view = "tab_main"
                             
                         }
                         if let result_refresh = result["refresh_token"].string{
                             UserDefaults.standard.set(result_refresh, forKey: "refresh_token")
-                            go_to_main.toggle()
+                            self.view_router.init_root_view = "tab_main"
                             
                         }
                     }
                 }else if(result_json == "token expired"){
                     print("토큰 expired ")
                     
-                    self.go_to_login.toggle()
+                    self.view_router.init_root_view = "origin"
                 }
                 //리프레시 토큰이 만료된 경우 다시 로그인해서 발급 받도록 한다.
                 // 일반/카카오/애플 회원인지 파악해 해당 api의 토큰도 확인
                 else if result_json == "refresh token expired"{
-                    go_to_login.toggle()
+                    self.view_router.init_root_view = "origin"
                     
                     print("refresh 토큰 expired")
                     let type = result["type"].string
@@ -130,17 +136,15 @@ struct SplashView: View {
                     }
                 }else{
                     print("토큰 그 외 오류 ")
-                    go_to_login.toggle()
+                    self.view_router.init_root_view = "origin"
                     
                 }
                 //서버로부터 response도 안왔을 때
             }else{
                 print("스플래시 통신 오류")
-                go_to_login.toggle()
+                self.view_router.init_root_view = "origin"
 
             }
         })
     }
 }
-
-
